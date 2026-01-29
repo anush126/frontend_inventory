8c0177e12fbcc3946a23148e094822fcbd1aaee1
anush126 <anushsubedar007@gmail.com>
2026-01-30 00:27:11 +0530

    port

--- src/api.js
+++ src/api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://backend-inventory-sfi9.onrender.com/api"
+    : "http://localhost:5000/api";
+
 export default axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL
 });
